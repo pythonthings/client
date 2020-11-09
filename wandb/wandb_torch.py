624216ea1e6a6369dd08254c741d2ba207346bb2
Fariz Rahman <farizrahman4u@gmail.com>
2020-11-09 16:23:35 +0400

    log

--- wandb/wandb_torch.py
+++ wandb/wandb_torch.py
@@ -62,6 +62,8 @@ def log_track_init(log_freq):
 def log_track_update(log_track):
     """count (log_track[0]) up to threshold (log_track[1]), reset count (log_track[0]) and return true when reached
     """
+    wandb.termlog("log_track_update()")
+    wandb.termlog(str(log_track))
     log_track[LOG_TRACK_COUNT] += 1
     if log_track[LOG_TRACK_COUNT] < log_track[LOG_TRACK_THRESHOLD]:
         return False
